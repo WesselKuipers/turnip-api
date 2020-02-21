71c30bb9f859558271dafbb4134d4cde13b1cec9
Wessel Kuipers <wesselkuipers@gmail.com>
2020-02-21 22:56:35 +0100

    setup environment settings and db connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
+import dotenv from 'dotenv';
+import fs from 'fs';
 import Koa from 'koa';
 import json from 'koa-json';
 import logger from 'koa-logger';
+import { Client } from 'pg';
 
 import routes from './routes';
+import { ApiContext } from './types';
 
-const PORT = process.env.PORT || 3000;
+const production = process.env.NODE_ENV === 'production';
+if (!production) {
+  dotenv.config();
+}
 
-const app = new Koa();
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set.');
+}
+
+const setup = fs.readFileSync('setup.sql', 'utf8');
+
+const app = new Koa<{}, ApiContext>();
+const client = new Client({ connectionString: process.env.DATABASE_URL, ssl: production });
+client.connect().then(async () => {
+  await client.query(setup);
+});
+
+app.context.db = client;
 
 // Middlewares
 app.use(json());
@@ -16,6 +35,7 @@ app.use(logger());
 app.use(routes.routes());
 app.use(routes.allowedMethods());
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening on port ${PORT}`);
