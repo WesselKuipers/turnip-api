d289c0538c0f45ce69ceae820a8cc8021c6a4dda
Wessel Kuipers <wesselkuipers@gmail.com>
2020-03-08 02:42:01 +0100

    normalized output of GET and POST

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -10,6 +10,19 @@ import {
   savePrice,
 } from '../utils';
 
+const descriptions: Record<ReturnType<typeof analyze>, string> = {
+  unknown:
+    'Unknown pattern. Either no pattern could be found with the current set of data or the pattern itself is invalid.',
+  decreasing:
+    'In the decreasing price pattern, the prices will consistently decrease by a few bells each time, never increasing. With the decreasing pattern, the start price for the week will be between 99 and 50 bells. In this pattern, if the price doesn’t increase by Thursday afternoon, you should sell immediately.',
+  random:
+    'The random price pattern features unpredictable prices in the range of about 50 to 200. The prices increase and decrease at random. The price seems to go over 110 at least twice a week in the random pattern.',
+  spikeBig:
+    'The big spike pattern features a decreasing pattern, but then there will be three increasing prices, with the third being the maximum for the week, followed by two decreasing prices that are still higher than average, followed by a continuation of the overall decreasing pattern. The maximum price in this pattern is always preceded by two increasing prices and always followed by two decreasing prices, so the maximum can’t occur on Monday or Saturday.',
+  spikeSmall:
+    'The small spike pattern features a decreasing pattern, but then there will be four increasing prices, with the fourth being the maximum for the week, followed by a decreasing price that is higher than average, followed by a continuation of the overall decreasing pattern. The maximum price in this pattern is always preceded by three increasing prices and followed by one decreasing price, so the earliest that the maximum can occur is Tuesday afternoon and the latest is Saturday morning.',
+};
+
 const router = new Router<{}, ApiContext>();
 const snowflakeRegex = /\d{17}/;
 
@@ -34,9 +47,13 @@ router.get('/:userId/turnips', async ctx => {
     ctx.throw(404, 'Record not found.');
   }
 
+  const prices = getPricesFromRecord(record);
+  const pattern = analyze(prices);
+
   ctx.body = {
-    record,
-    prices: getPricesFromRecord(record),
+    ...record,
+    prices,
+    pattern: { type: pattern, description: descriptions[pattern] },
   };
 });
 
@@ -81,7 +98,14 @@ router.post('/:userId/turnips', async ctx => {
     await savePrice(db, currentRecord);
   }
 
-  ctx.body = { week, year, day, period, userId, body: ctx.request.body, currentRecord };
+  const prices = getPricesFromRecord(currentRecord);
+  const pattern = analyze(prices);
+
+  ctx.body = {
+    ...currentRecord,
+    prices,
+    pattern: { type: pattern, description: descriptions[pattern] },
+  };
 });
 
 router.get('/:userId/turnips/pattern', async ctx => {
